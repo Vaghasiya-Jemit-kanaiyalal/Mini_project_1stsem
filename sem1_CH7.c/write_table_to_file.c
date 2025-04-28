7f0f4178768ff12ca7124eded6e4f6a1e56ccd6d
Jemit_Vaghasiya <jemitvaghasiya07@gmail.com>
2025-04-28 22:44:40 +0530

    Update and rename quiz2.c to write_table_to_file.c

--- sem1_CH7.c/write_table_to_file.c
+++ sem1_CH7.c/write_table_to_file.c
@@ -19,3 +19,14 @@ int main()
     fclose(ptr); // Close the file after use
     return 0;
 }
+//OUTPUT :
+/*5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50*/
